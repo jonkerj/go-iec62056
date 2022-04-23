aacacc4cb3f97c955161f6f9d51b10275358dbd4
Jorik Jonker <jorik@kippendief.biz>
2022-04-23 09:55:30 +0200

    feat: remove DSMR scanner
    This does not belong in a IEC62056 parser; it will be maintained as a
    part of `gonuts` instead.

--- main.go
+++ main.go
@@ -2,38 +2,19 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/jonkerj/go-iec62056/pkg/parser"
-	"github.com/jonkerj/go-iec62056/pkg/scanner"
-	"go.bug.st/serial"
+	"github.com/jonkerj/go-iec62056/pkg/samples"
 )
 
-func telegramCallback(datagram []byte, checksum uint16) {
-	telegram, err := parser.Parse(datagram)
+func main() {
+	telegram, err := parser.Parse(samples.Ziv_5CTA3[:714]) // 714 is the `!` in the datagram
 	if err != nil {
 		panic(fmt.Sprintf("parse error: %v", err))
 	}
 
 	fmt.Printf("telegram: %v\n", telegram)
 	for _, obj := range telegram.Objects {
-		fmt.Printf("obj: %v\n", obj)
+		fmt.Printf("obj: %v (%T)\n", obj, obj.Value)
 	}
-}
-
-func main() {
-	mode := &serial.Mode{
-		BaudRate: 115200,
-		Parity:   serial.NoParity,
-		DataBits: 8,
-		StopBits: serial.OneStopBit,
-	}
-	port, err := serial.Open("/dev/ttyUSB0", mode)
-	if err != nil {
-		panic(err)
-	}
-	port.SetReadTimeout(10 * time.Millisecond)
-
-	scanner := scanner.NewDSMRScanner(port, telegramCallback)
-	scanner.ReadLoop()
 }
